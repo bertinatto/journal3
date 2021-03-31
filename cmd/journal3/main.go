048d5d7c7a832cad5722d6eaeebc993c0fe6adcd
Fabio Bertinatto <fbertina@redhat.com>
2021-03-31 07:50:37 +0200

    Parse cli args

--- cmd/journal3/main.go
+++ cmd/journal3/main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"context"
+	"flag"
+	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 
 	"github.com/bertinatto/journal3/http"
 	"github.com/bertinatto/journal3/sqlite"
 	"k8s.io/klog"
 )
 
+const (
+	defaultDataFile = "data.db"
+	defaultAddress  = "127.0.0.1:1111"
+)
+
 func main() {
-	db := sqlite.NewDB("blog.db")
-	err := db.Open()
+	file := flag.String("file", defaultDataFile, "file where data will persist")
+	addr := flag.String("listen", defaultAddress, "ip:port")
+	flag.Parse()
+
+	dir := filepath.Dir(*file)
+	_, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		log.Printf("isnotexist")
+		err := os.Mkdir(dir, 750)
+		if err != nil {
+			klog.Fatal(err)
+		}
+
+		log.Printf("created %v", dir)
+	}
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	db := sqlite.NewDB(*file)
+	err = db.Open()
 	if err != nil {
 		klog.Fatal(err)
 	}
@@ -31,7 +58,7 @@ func main() {
 	}()
 
 	klog.Infof("Starting the HTTP server")
-	err = s.Open("127.0.0.1:1111")
+	err = s.Open(*addr)
 	if err != nil {
 		klog.Fatal(err)
 	}
