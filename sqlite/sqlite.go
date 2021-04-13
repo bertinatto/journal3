a6f4cdaab77c75d74dbe2de5942d3b187e9346cc
Fabio Bertinatto <fbertina@redhat.com>
2021-04-13 07:38:14 +0200

    Add DB migration logic

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type Tx struct {
+	*sql.Tx
+	now time.Time
+}
 
 type DB struct {
 	db     *sql.DB
@@ -42,6 +50,11 @@ func (db *DB) Open() error {
 		return fmt.Errorf("could not enable WAL: %w", err)
 	}
 
+	err = db.migrate()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -65,7 +78,60 @@ func (db *DB) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
 	}, nil
 }
 
-type Tx struct {
-	*sql.Tx
-	now time.Time
+func (db *DB) migrate() error {
+	_, err := db.db.Exec(`CREATE TABLE IF NOT EXISTS migrations (name TEXT PRIMARY KEY);`)
+	if err != nil {
+		return fmt.Errorf("failed to create migrations table: %w", err)
+	}
+
+	names, err := filepath.Glob("sqlite/migration/*.sql")
+	if err != nil {
+		return err
+	}
+	sort.Strings(names)
+
+	if len(names) == 0 {
+		return fmt.Errorf("failed to find at least one migration file")
+	}
+
+	for _, name := range names {
+		err := db.migrateFile(name)
+		if err != nil {
+			return fmt.Errorf("failed to execute migration %q: %w", name, err)
+		}
+	}
+	return nil
+}
+
+func (db *DB) migrateFile(name string) error {
+	tx, err := db.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var n int
+	err = tx.QueryRow(`SELECT COUNT(*) FROM migrations WHERE name = ?`, name).Scan(&n)
+	if err != nil {
+		return err
+	}
+	if n != 0 {
+		return nil
+	}
+
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(string(buf))
+	if err != nil {
+		return err
+	}
+
+	if _, err := tx.Exec(`INSERT INTO migrations (name) VALUES (?)`, name); err != nil {
+		return err
+	}
+
+	return tx.Commit()
 }
